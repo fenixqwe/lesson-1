62efeb61cdb8edb3381107d73862ad753f3c1aa4
fenixqwe <53240682+fenixqwe@users.noreply.github.com>
2019-09-27 15:16:19 +0300

    First Presentation

--- app.js
+++ app.js
@@ -1,102 +1,122 @@
-//Что отобразится в консоли. Почему?
-
-//12 + 14 + '12' - отобразится 2612 , потому-что выполниться сначало действие (12 + 14) а потом уже это число прибавиться к строке;
-//3 + 2 - '1' - отобразится 4
-//true + 2 - отобразится 3 , потому-что true = 1;
+let string = 'some test string';
+//1. Получить первую и последнюю буквы строки
 
 
-//+'10' + 1 - отобразится 11;
+/* Последняя буква
+value = string[string.length - 1]
 
-//undefined + 2 - отобразится NaN;
+console.log (value)
+*/
+/* Первая буква 
+value = string[0];
 
-
-//null + 5 - отобразится 5;
-
-
-//true + undefined - отобразится NaN
-//1) Если переменная равна “hidden”, присвоить ей значение “visible”, иначе - “hidden”.
+console.log (value)
 /*
-let a;
 
-if (a == 'hidden'){
-	a = 'visible';
-}
-else{
-	a = 'hidden';
-}
+//2. Сделать первую и последнюю буквы в верхнем регистре
+
+/* Последняя буква 
+value = string[string.length - 1]
+
+console.log (value.toUpperCase())*/
+
+/* Первая буква 
+value = string[0];
+
+console.log(value.toUpperCase())*/
+//3. Найти положение слова ‘string’ в строке
 
 
-console.log (a)
-*/
-/*2)Используя if, записать условие:
-1. если переменная равна нулю, присвоить ей 1;
-2. если меньше нуля - строку “less then zero”;
-3. если больше нуля - используя оператор “присвоение”, переменную умножить на 10 (использовать краткую запись).*/
 /*
-let a;
+value = string.indexOf('string');
 
-if (a == 0){
-	a = 1
-}
-if (a < 0){
-	a = 'less then zero'
-}
-if (a > 0){
-	a *= 10;
-}
-
-console.log(a)
+console.log(value)
 */
-/*3. Дан объект let car = { name: 'Lexus', age: 10, create: 2008, needRepair: false }. 
-Написать условие если возраст машины больше 5 лет то нужно вывести в консоль сообщение 'Need Repair' и свойство needRepair в объекте car изменить на true; иначе изменить на false.*/
+//4. Найти положение второго пробела (“вручную” ничего не считать)
 
-/*let car = { 
-	name: 'Lexus', 
-	age: 10, 
-	create: 2008,
-	needRepair: false 
-}
-if (car.age > 5){
-	console.log("Need Repair")
-	car.needRepair = true;
-}
-else{
-	car.needRepair = false;
-}
 
-console.log(car)
+/*
+secondSpace = string.indexOf(' ', 5);
+
+console.log(secondSpace)
 */
-//4. Дан объект let item = { name: 'Intel core i7', price: '100$', discount: '15%' }.
-/*let item = { 
-	name: 'Intel core i7',
- 	price: '100$',
-  	discount: '15%' 
+//5. Получить строку с 5-го символа длиной 4 буквы
 
-}
-if (item.discount && parseInt(item.discount)){
-	item.priceWithDiscount = (100 - (100 * parseInt(item.discount) / 100))
-	value = item.priceWithDiscount
-}	
-else{
-	console.log(item.price)
-}
+
+/*let value = string.substr(4, 5)
+
 console.log(value)*/
-//Написать условие если (цена товара больше или равна минимальной цене) и (меньше или равна максимальной цене) то вывести в консоль название этого товара, 
-//иначе вывести в консоль что товаров не найдено.
+//6. Получить строку с 5-го по 9-й символы
 
-/*let product = {
-    name: 'Яблоко',
-    price: '10$'
-};
 
-let min = 10; 
-let max = 20;
-let price = product.price;
+/*let value = string.slice(4, 8)
 
-if (parseInt(price) >= min && parseInt(price) <= max){
+console.log(value)*/
+/*7. Получить новую строку из исходной путем удаления последних 6-и символов
+(то есть исходная строка без последних 6и символов)*/
 
-	console.log(product.name);
-}
-else{
-	console.log('товаров не найдено')
-}*/
+
+/*
+value = string.slice(0, -6);
+
+
+console.log(value)*/
+//8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016”
+
+
+/*const a = 20;
+const b = 16;
+
+console.log(String(a) + String(b))*/
+
+//let a = 0 || 'string';  ‘string’;  0 - false, ‘string’ - true
+
+//let a = 1 && 'string'; Answer - ('string'); 1 - true; string - true;
+
+
+//let a =  null || 25; Answer - (25)
+ 
+//let a = null && 25; Answer - (null)
+
+
+//let a = null || 0 || 35; Answer - (35)
+
+//let a = null && 0 && 35; Answer - (null)
+
+//1. Получить число pi из Math и округлить его до 2-х знаков после точки
+
+//console.log(Math.PI.toFixed(2))
+
+//2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
+/*
+let min = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+let max = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+
+console.log(min, max)*/
+
+//Получить случайное число и округлить его до двух цифр после запятой
+
+/*let value = Math.random().toFixed(2);
+
+console.log(value)*/
+//Получить случайное целое число от 0 до X. X - любое произвольное число.
+
+/*let a = 10;
+let value = Math.random() * 10;
+let x = Math.floor(value)
+
+console.log(x)*/
+//4. Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду (1.3)?
+
+/*let a = 0.6 + 0.7;
+
+console.log(a.toFixed(1))*/
+
+//5. Получить число из строки ‘100$’
+/*
+let x = '100$';
+
+let a = parseInt(x) + 5;
+
+console.log(a)*/
+
